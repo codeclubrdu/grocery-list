844271c3c5d64139974756d0d9debb068dd392fe
mspaits <mspaits@gmail.com>
2026-07-29 20:57:12 -0400

    Got things to delete from the list one at a time.  Not optimal.

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { useState } from 'react';
 import AddToList from '@/app/components/AddToList.jsx';
 import IntroStatement from '@/app/components/introStatement.jsx';
+import { Preahvihear } from 'next/font/google';
 
 type groceryObject = {
 	name: string;
@@ -49,20 +50,10 @@ const groceryList: groceryObject[] = [
 		section: 'deli',
 		store: 'Wegmans',
 	},
-].sort((a, b) => {
-	const sectionA = a.section.toUpperCase();
-	const sectionB = b.section.toUpperCase();
-	if (sectionA < sectionB) {
-		return -1;
-	}
-	if (sectionA > sectionB) {
-		return 1;
-	}
-	return 0;
-});
+];
 
 // Component to render the list. For some reason I left it in with the main page component.
-export function ToBuyList({ listToRender }: { listToRender: groceryObject[] }) {
+export function ToBuyList({ listToRender, saveChecks }: { listToRender: groceryObject[] }) {
 	// Create a list of each store with no duplicates
 	// Get an array containing only the stores
 	let storeList = listToRender.map((listItem) => listItem.store);
@@ -93,7 +84,9 @@ export function ToBuyList({ listToRender }: { listToRender: groceryObject[] }) {
 														<span className="size-4 rounded-full bg-white shadow-xs ring-1 ring-gray-900/5 transition-transform duration-200 ease-in-out group-has-checked:translate-x-3.5"></span>
 														<input
 															type="checkbox"
-															id="{listItem.name}"
+															name={listItem.name}
+
+															onChange={saveChecks}
 															className="absolute inset-0 size-full appearance-none focus:outline-hidden"
 														></input>
 													</div>
@@ -124,7 +117,56 @@ export default function Home() {
 			store: formData.get('store') as string,
 		};
 
-		setList([...list, newGrocery]);
+		// set list to add new item and sort first by section then by store
+		setList(
+			[...list, newGrocery]
+				.sort((a, b) => {
+					const sectionA = a.section.toUpperCase();
+					const sectionB = b.section.toUpperCase();
+					if (sectionA < sectionB) {
+						return -1;
+					}
+					if (sectionA > sectionB) {
+						return 1;
+					}
+					return 0;
+				})
+				.sort((a, b) => {
+					const storeA = a.store.toUpperCase();
+					const storeB = b.store.toUpperCase();
+					if (storeA < storeB) {
+						return -1;
+					}
+					if (storeA > storeB) {
+						return 1;
+					}
+					return 0;
+				}),
+		);
+	}
+
+	// Keeping track of which checkboxes are checked
+	const [checkedList, setCheckedList] = useState({});
+
+	function saveCheckState(e) {
+		const name = e.target.name;
+		const isChecked = e.target.checked;
+		setCheckedList((prev) => ({ ...prev, [name]: isChecked }));
+	}
+	console.log(checkedList);
+
+	// Function to delete checked items from list
+	// Dude.  Just add an isChecked property to the list items and manage through that.
+	function deleteChecks() {
+		for (const item of list) {
+			//console.log(item);
+			if (item.name in checkedList && checkedList[item.name]) {
+				console.log(item.name);
+				const updatedList = list.filter((grocery) => grocery['name'] != item.name);
+				console.log(updatedList);
+				setList(updatedList);
+			}
+		}
 	}
 
 	return (
@@ -133,8 +175,8 @@ export default function Home() {
 				Grocery List
 			</h1>
 			<IntroStatement sponsor={"Carl's Jr."}></IntroStatement>
-			<ToBuyList listToRender={list}></ToBuyList>
-			<AddToList handleSubmit={addItem}></AddToList>
+			<ToBuyList listToRender={list} saveChecks={saveCheckState}></ToBuyList>
+			<AddToList handleSubmit={addItem} removeChecks={deleteChecks}></AddToList>
 		</>
 	);
 }
