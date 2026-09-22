7b7130a05362782ac24325e7d9743d0b81f9f7fd
mspaits <mspaits@gmail.com>
2026-09-21 20:15:26 -0400

    Changed font on footer

--- app/components/IntroStatement.tsx
+++ app/components/IntroStatement.tsx
@@ -1,12 +1,18 @@
 import Image from 'next/image';
+import { Short_Stack } from 'next/font/google';
+
+const shortStack = Short_Stack({
+	weight: '400',
+	subsets: ['latin'],
+});
 
 export default function IntroStatement({ sponsor }: { sponsor: string }) {
 	return (
-		<div className="flex flex-row justify-center gap-5 bg-zinc-100 p-2 text-gray-500">
-			<p className="flex flex-col justify-center">
-				Brought to you by {sponsor} <br />
+		<div className="flex flex-row justify-center gap-4 bg-zinc-100 p-2 text-gray-500">
+			<p className={`${shortStack.className} flex flex-col justify-center`}>
+				Brought to you by {sponsor} and
+				<br />
 			</p>
-			<p className="flex flex-col justify-center">AND</p>
 			<Image src="/codeClubLogo.png" alt="Code Club RDU logo" width={100} height={100} />
 		</div>
 	);
